b63c263fb23e8703a960752a672580bd38ed3e26
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-13 19:16:50 +0300

    - PerformanceLogProperties для понимания пропертей

--- src/main/java/com/company/app/core/aop/logging/performance/component/config/PerformanceLogProperties.java
+++ src/main/java/com/company/app/core/aop/logging/performance/component/config/PerformanceLogProperties.java
@@ -2,13 +2,11 @@ package com.company.app.core.aop.logging.performance.component.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * автокомплит application.properties только в Ultimate IDEA
  */
 @Data
-@Configuration
 @ConfigurationProperties(prefix = "performance.log.annotation")
 public class PerformanceLogProperties {
 
