228b8b2d381f67e5478ef1e9144a2392668e76d3
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-14 10:47:29 +0300

    - info про yml

--- src/test/java/com/company/app/springboot/application/SpringBootApplicationTest.java
+++ src/test/java/com/company/app/springboot/application/SpringBootApplicationTest.java
@@ -6,6 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.TestPropertySource;
 
+/**
+ * yml можно указать профилем
+ * @ActiveProfiles("test") + application-test.yml
+ * или
+ * вот так, но я не проверял еще:
+ * @TestPropertySource(properties = {"spring.config.location = src/test/resources/test.yml"})
+ */
 @SpringBootTest(
 		classes = StarterConfiguration.class
 )
