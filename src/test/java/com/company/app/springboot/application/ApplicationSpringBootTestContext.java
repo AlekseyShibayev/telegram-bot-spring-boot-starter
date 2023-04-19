a2b55bb32624ba56d37f3bdbbb6455b55cc4618b
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-20 00:50:22 +0300

    - ref ApplicationSpringBootTestContext

--- src/test/java/com/company/app/springboot/application/ApplicationSpringBootTestContext.java
+++ src/test/java/com/company/app/springboot/application/ApplicationSpringBootTestContext.java
@@ -1,10 +1,13 @@
 package com.company.app.springboot.application;
 
 import com.company.app.core.StarterConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.TestPropertySource;
+
+import javax.annotation.PostConstruct;
 
 /**
  * yml можно указать профилем
@@ -14,10 +17,17 @@ import org.springframework.test.context.TestPropertySource;
  * вот так, но я не проверял еще:
  * @TestPropertySource(properties = {"spring.config.location = src/test/resources/test.yml"})
  */
+
+@Slf4j
 @SpringBootTest(
 		classes = StarterConfiguration.class
 )
 @ExtendWith(OutputCaptureExtension.class)
 @TestPropertySource("/test.properties")
 public abstract class ApplicationSpringBootTestContext {
+
+	@PostConstruct
+	void init() {
+		log.debug("**********     запущена группа тестов всего приложения     **********");
+	}
 }
