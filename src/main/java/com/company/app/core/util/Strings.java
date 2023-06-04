863b1dc9f9ea3b5fb74b61566577a336a70663ac
shibaev <mail9788615101@gmail.com>
2023-06-04 12:50:34 +0300

    - DEFAULT_DELIMITER

--- src/main/java/com/company/app/core/util/Strings.java
+++ src/main/java/com/company/app/core/util/Strings.java
@@ -5,6 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Strings {
 
+	public static final String DEFAULT_DELIMITER = ";";
+
 	public static String cutEnd(String string, int amount) {
 		try {
 			return string.substring(0, string.length() - amount);
